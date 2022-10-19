97d6e5b18d97fb9db276a382a4a7a9816cf8a9f5
subramanian <subramanian.nk@live.com>
2022-10-18 23:57:50 -0500

    added activity in the reciever to see if anything is propagated

--- Experimenting/RabbitListener.cs
+++ Experimenting/RabbitListener.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -16,10 +17,18 @@ namespace Experimenting
         [Function("RabbitListener")]
         public void Run([RabbitMQTrigger("myqueue", ConnectionStringSetting = "amqp://guest:guest@localhost:15672/")] string myQueueItem, FunctionContext Context)
         {
+            var activity = new Activity("Parent");
+            //var guid = "00-" + parentId + "-15f670e22f6dbece-00";
+            //activity.SetParentId(guid);
+            activity.Start();
+            var t = activity.Id;
+            activity.SetParentId(t);
+
             var (operationId, parentId) = GetOperationIdAndParentId(Context);
             _logger.LogInformation($"The operation Id is : {parentId}");
             // amqps://gmtxfped:q9q2CrgirKMatFilFe-qlM4cMzGa4hii@beaver.rmq.cloudamqp.com/gmtxfped
-           
+
+            activity.Stop();
         }
 
         private (string, string) GetOperationIdAndParentId(FunctionContext executionContext)
